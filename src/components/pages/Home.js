1c883a17ae3d3fce354be4b1d8651f8a27cacbaa
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-10-08 23:45:33 -0400

    link search page to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import BookShelf from '../BookShelf'
 
 class Home extends React.Component {
@@ -14,7 +15,7 @@ class Home extends React.Component {
         </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to="/Search">Add a book</Link>
         </div>
       </div>
     );
