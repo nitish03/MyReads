0e987542c1985f08fb28f03084990820896b2b2a
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-10-09 08:56:00 -0400

    add map method to get books grid

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -11,7 +11,7 @@ class BookShelf extends React.Component {
         <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-           <Book/>
+           {this.props.books.map((book) => <Book book={book} key={book.id}/>)}
           </ol>
         </div>
       </div>
